1d6fb392182f0f532fd77b5119f3be5c97769e0b
linqifa0428 <linqifa2024@163.com>
2025-04-17 16:42:09 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from model import Network
+from model import CNN
 from torchvision import transforms
 from torchvision import datasets
 import torch
@@ -12,11 +12,13 @@ if __name__ == '__main__':
     test_dataset = datasets.ImageFolder(root='../MNIST-Test/mnist_images/train',transform=transform)
     print("test_dataset length: ", len(test_dataset))
 
-    model = Network()
+    model = CNN()
+    model.eval()
     model.load_state_dict(torch.load('mnist.pth'))
 
     right = 0
     for i, (x, y) in enumerate(test_dataset):
+        x = x.unsqueeze(0)  # 添加batch维度,从[1,28,28]变为[1,1,28,28]
         output = model(x)
         predict = output.argmax(1).item()
         if predict == y:
