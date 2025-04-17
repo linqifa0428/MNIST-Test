a41d9976c4d85c12f71b5f789a08f0e910e1f4a4
linqifa0428 <linqifa2024@163.com>
2025-04-17 16:47:53 +0800

    Add files via upload

--- dataloader.py
+++ dataloader.py
@@ -1,8 +1,9 @@
 # 这个程序的功能会先将MNIST数据下载下来，然后再保存为.png的格式。
-
+import os
+import secrets
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor
-
+from torchvision.transforms import ToPILImage
 # 注意，这里得到的train_data和test_data已经直接可以用于训练了！
 # 不一定要继续后面的保存图像。
 train_data = MNIST(root='./data', train=True, download=True, transform=ToTensor())
@@ -12,14 +13,10 @@ test_data = MNIST(root='./data', train=False, download=True, transform=ToTensor(
 # 输出两个文件夹，train和test，分别保存训练和测试数据。
 # train和test文件夹中，分别有0、1、2、3、4、5、6、7、8、9；
 # 这10个子文件夹保存每种数字的数据
-
-from torchvision.transforms import ToPILImage
-
 train_data = [(ToPILImage()(img), label) for img, label in train_data]
 test_data = [(ToPILImage()(img), label) for img, label in test_data]
 
-import os
-import secrets
+
 def save_images(dataset, folder_name):
     root_dir = os.path.join('./mnist_images', folder_name)
     os.makedirs(root_dir, exist_ok=True)
@@ -29,6 +26,7 @@ def save_images(dataset, folder_name):
         os.makedirs(label_dir, exist_ok=True)
         random_filename = secrets.token_hex(8) + '.png'
         img.save(os.path.join(label_dir, random_filename))
+
 
 save_images(train_data, 'train')
 save_images(test_data, 'test')
